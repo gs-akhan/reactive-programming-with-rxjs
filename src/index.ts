87920119b555e469db6637d3b6a320e2c9f713f3
Jagadeesh S <siripurapujk@yahoo.com>
2017-04-02 13:28:33 +0530

    rxjs setup

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+
+import * as Hapi from "hapi";
+import * as Inert from "inert";
+import * as Susie from "susie";
+import * as Path from "path";
+
+let server = new Hapi.Server();
+
+server.connection({
+    address: "localhost",
+    port: process.env.PORT || '8085',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        }
+    }
+});
+
+let inert = server.register(Inert);
+let susie = server.register(Susie);
+
+Promise.all([inert, susie]).then(() => {
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: true,
+                index: true
+            }
+        }
+    });
+
+    server.route({
+        path: "/events",
+        method: "GET",
+        handler: (req, reply) => {
+            reply["event"]({ id: 1, data: 'my data' });
+            reply["event"](null);
+        }
+    });
+
+    server
+        .start()
+        .then(() => console.info("Server started.."))
+        .catch((err) => console.error(err));
+
+});
