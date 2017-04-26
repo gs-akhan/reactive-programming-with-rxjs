1832ed954496e597d1b12681d254fae73b8b9b41
Jagadeesh S <siripurapujk@yahoo.com>
2017-04-26 23:26:11 +0530

    updated menu bar

--- src/client/example-00/basics/map.ts
+++ src/client/example-00/basics/map.ts
@@ -8,18 +8,19 @@ export module map{
     let _name = "Map";
     
     export function init(){       
-        let colors = helper.colors.concat([]); 
+        
         let menuItem = helper.menu.replace(/\$\{NAME\}/ig, _name);
         return {
             name:_name,
             menuItem,
             start(){
+                let colors = helper.colors.concat([]); 
                 _start = true;
                 return Observable
                             .interval(1000)
                             .map(v=>({color:colors.shift(), x:20, txt:"S1:"+v}))
                             .takeWhile(v=>_start)
-                            .take(10)                            
+                            .take(10);                         
             },
             stop(){
                 _start = false;
